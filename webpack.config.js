c7391908e153c31bc5cbc566421f225604f7ae9b
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-05-16 12:39:50 +0300

    Add css-loader and style-css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
     module: {
@@ -17,6 +16,10 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
